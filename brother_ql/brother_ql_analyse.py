e27bc9d491e98d07b3ac9f9da6abacdbcbefb562
Philipp Klaus <philipp.l.klaus@web.de>
2017-10-18 23:27:55 +0200

    brother_ql_analyze can now read from stdin (with '-')

--- brother_ql/brother_ql_analyse.py
+++ brother_ql/brother_ql_analyse.py
@@ -7,11 +7,16 @@ from brother_ql.reader import BrotherQLReader
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='The file to analyze')
+    parser.add_argument('file', help='The file to analyze', type=argparse.FileType('rb'))
     parser.add_argument('--loglevel', type=lambda x: getattr(logging, x), default=logging.WARNING, help='The loglevel to apply')
     args = parser.parse_args()
 
     logging.basicConfig(stream=sys.stdout, format='%(levelname)s: %(message)s', level=args.loglevel)
+
+    try:
+        args.file = args.file.buffer
+    except AttributeError:
+        pass
 
     br = BrotherQLReader(args.file)
     br.analyse()
